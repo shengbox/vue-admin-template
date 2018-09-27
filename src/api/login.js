d466e79189490aff087fe3edb09592a31ec02fbd
zhangsheng <shengbox@gmail.com>
2018-09-27 19:19:28 +0800

    test

--- src/api/login.js
+++ src/api/login.js
@@ -28,26 +28,16 @@ export function logout() {
 }
 
 export function oauth(user, pass) {
-  var oauth_token = 'https://api.joyowo.com/cloud-auth/oauth/token'
-  var basicAuth = 'Basic ' + btoa('crm-wx' + ':' + 'tianwu_crm')
   return axios({
     method: 'post',
-    url: oauth_token,
+    url: 'https://api.joyowo.com/cloud-auth/oauth/token',
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': basicAuth
+      'Authorization': 'Basic ' + btoa('crm-wx' + ':' + 'tianwu_crm')
     },
     data: {
       grant_type: 'password',
       username: user,
       password: pass
-    },
-    transformRequest: [function(data) {
-      let ret = ''
-      for (const it in data) {
-        ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
-      }
-      return ret
-    }]
+    }
   })
 }
