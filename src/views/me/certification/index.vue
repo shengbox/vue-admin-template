9d4d4d2c3d8cd28272ab65b32bc9cc1f7b15a1d1
zhangsheng <shengbox@gmail.com>
2018-10-14 13:56:42 +0800

    longbo

--- src/views/me/certification/index.vue
+++ src/views/me/certification/index.vue
@@ -1,28 +1,23 @@
 <template>
   <div class="app-container">
-    <el-row type="flex" justify="center">
-      <el-col :span="18">
-        <h3 style="float: left;">个人认证</h3><img src="http://ism.yonyou.com/appstore/OCS-004/logo/OCS-004.png" style="width: 40px">
-      </el-col>
-    </el-row>
-    <el-row type="flex" justify="center">
-      <el-col :span="8">
-        真实姓名：{{ form.name }}
-      </el-col>
-      <el-col :span="8">
-        身份证号码：{{ form.ID_card_No }}
-      </el-col>
-    </el-row>
-    <br>
-    <el-row type="flex" justify="center">
-      <el-col :span="18"><h3>企业认证</h3></el-col>
-    </el-row>
-    <el-row type="flex" justify="center">
-      <el-col :span="16">公司名称：{{ form.company }}</el-col>
-    </el-row>
-    <el-row type="flex" justify="center">
-      <el-col :span="16">统一社会信用代码：{{ form.company }}</el-col>
-    </el-row>
+    <el-form ref="form" :model="form" label-width="140px">
+      <el-row>
+        <el-col :span="18">
+          <h3 style="display: inline-block">个人认证</h3><img src="http://ism.yonyou.com/appstore/OCS-004/logo/OCS-004.png" style="width: 40px;margin-bottom: -8px;">
+        </el-col>
+      </el-row>
+      <el-form-item label="真实姓名:"> {{ form.name }} </el-form-item>
+      <el-form-item label="身份证号码:"> {{ form.ID_card_No }} </el-form-item>
+      <el-row>
+        <el-col><h3>企业认证</h3></el-col>
+      </el-row>
+      <el-form-item label="公司名称"> {{ form.company }} </el-form-item>
+      <el-form-item label="统一社会信用代码"> {{ form.companyUniteNo }} </el-form-item>
+      <el-form-item label="法人身份证号码"> {{ form.idCardNo }} </el-form-item>
+      <el-form-item label="法人姓名"> {{ form.name }} </el-form-item>
+      <el-form-item label="对公银行账户"> {{ form.bankCardNo }} </el-form-item>
+      <el-form-item label="开户行"> {{ form.bankName }} </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -34,10 +29,12 @@ export default {
         name: '張盛',
         ID_card_No: '33068*******5621',
         company: '杭州仙人掌科技有限公司',
-        date2: '',
+        companyUniteNo: '91330104MA2CD0AL8C',
+        bankCardNo: '03335100040029654',
+        idCardNo: '210902197601042517',
         delivery: false,
         type: [],
-        resource: '',
+        bankName: '中国农业银行',
         desc: ''
       }
     }
